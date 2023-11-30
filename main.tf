63e54feec3907031a426e05cd13eac0c4d13c6e0
Dominic Singaraj <dominic.singaraj.deutschland@gmail.com>
2023-11-30 18:16:47 +0100

    Added Route tables and Internet Gateway

--- main.tf
+++ main.tf
@@ -46,3 +46,59 @@ resource "aws_subnet" "private-2" {
     Name = "deham9" 
   }
 }
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "deham9"
+  }
+}
+
+
+resource "aws_route_table" "RB_Public_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = var.CIDR_BLOCK
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "deham9"
+  }
+}
+/*
+resource "aws_route_table" "RB_Private_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = var.CIDR_BLOCK
+    gateway_id = "local"
+  }
+  tags = {
+    Name = "deham9"
+  }
+}*/
+resource "aws_route_table_association" "Public_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-1.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
+}
+/*
+resource "aws_route_table_association" "Private_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-1.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
+}
+*/
+resource "aws_route_table_association" "Public_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-2.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
+}
+/*
+resource "aws_route_table_association" "Private_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-2.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
+}*/
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "cidr_block_RB_Public_Subnet1" {
+  default = "10.0.1.0/24"    #Public Subnet-1
+}
+
+variable "CIDR_BLOCK" {
+  default = "0.0.0.0/0"    #Public CIDR
+}
