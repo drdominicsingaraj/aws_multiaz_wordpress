7c4363a5db0baed4291fda64c5ef09b784e40047
Dominic Singaraj <dominic.singaraj.deutschland@gmail.com>
2023-11-30 17:40:56 +0100

    Added Subnets - public and private

--- main.tf
+++ main.tf
@@ -3,6 +3,46 @@ resource "aws_vpc" "dev_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags       =  {
-    name     = "dev_vpc"
+    name     = "deham9"
   }       
+}
+
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "deham9"
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "deham9" 
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "deham9"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "deham9" 
+  }
 }
